ff0b8ea402c0b25ad986bb131b3eda7ad0f6ba92
WADIRAHUL <rahulwadi019@gmail.com>
2022-07-16 20:52:16 +0530

    UPDATED 16/7/22

--- src/basics/LaunchingEmptyBrowser.java
+++ src/basics/LaunchingEmptyBrowser.java
@@ -1,14 +1,19 @@
 package basics;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LaunchingEmptyBrowser {
-	public static void main(String[] args) throws InterruptedException {
-		 System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		 ChromeDriver driver =new ChromeDriver();
+	public static void main(String[] args) throws Exception {
+		 System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		 WebDriver driver =new FirefoxDriver();
 		 Thread.sleep(1000);
-		 driver.get("https://mail.google.com/");
-		 driver.quit();
+		 driver.get("https://www.hotstar.com/");
+		 Thread.sleep(3000);
+		 driver.findElement(By.linkText("Latest & Trending")).click();
+		 Thread.sleep(5000);
+		 
 	}
 	
 }
